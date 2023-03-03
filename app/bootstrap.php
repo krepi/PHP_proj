cb820d4e852f7f7872e4e7e00a24daf60e3a0b4d
Przemek <krepski80@gmail.com>
2023-03-03 23:07:38 +0100

    user registration done, url_helper added , User model created

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,10 +2,9 @@
 //Load Cofig file
 require_once 'config/config.php';
 
-// Load libraries
-// require_once 'libraries/Core.php';
-// require_once 'libraries/Controller.php';
-// require_once 'libraries/Database.php';
+// Load helpers
+require_once 'helpers/url_helper.php';
+
 
 //Autoloader for libraries files
 
--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -4,6 +4,7 @@ class Users extends Controller
 {
     public function __construct()
     {
+        $this->userModel = $this->model('User');
     }
 
     public function register()
@@ -30,6 +31,11 @@ class Users extends Controller
             //Validate email
             if (empty($data['email'])) {
                 $data['email_err'] = 'Please enter email';
+            } else {
+                //check email
+                if($this->userModel->findUserByEmail($data['email'])){
+                    $data['email_err'] = 'Email is already taken';
+                }
             }
 
             //Validate name
@@ -56,7 +62,20 @@ class Users extends Controller
             // Make sure errors are empty
             if (empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])) {
                 //validated
-                die('success');
+               
+                //hash password
+                $data['password'] = password_hash( $data['password'] , PASSWORD_DEFAULT);
+
+                //Register User
+                
+                if($this->userModel->register($data)){
+                    //redirect
+                    redirect('/users/login');
+
+                } else {
+                    die('somethin were wrong');
+                }
+
             } else {
                 $this->view('users/register', $data);
             }
@@ -84,7 +103,7 @@ class Users extends Controller
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             //Process form
 
-            
+
             //Sanitize POST data
             $_POST = filter_input_array(htmlspecialchars(INPUT_POST));
 
